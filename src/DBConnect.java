88d0d8fdb4648c66a89bd72b3686601abdb55d0d
BerkantGC <berkantgurcan@hotmail.com>
2022-06-21 12:31:24 +0300

    ConnectProgram

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -1,51 +1,36 @@
 import java.sql.*;
 import java.util.*;
 
-public class DBConnect {
+public class DBConnector {
     public static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
     public static final String DB_USERNAME = "postgres";
     public static final String DB_PASSWORD = "root";
 
-    public static void main(String[] args) {
-        Connection conn = null;
-        Statement st = null;
+    private Connection connect = null;
+
+    public Connection connectDB()
+    {
         try {
-            conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            st = conn.createStatement();
+            this.connect = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_URL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return this.connect;
+    }
 
-            String areas = new String();
-            ResultSet resultSet = st.executeQuery("SELECT * " +
-                                                    "FROM film " +
-                                                    "WHERE film_id BETWEEN 10 AND 50");
+    public static Connection getInstance()
+    {
+        DBConnector db=  new DBConnector();
+        return db.connectDB();
+    }
 
-            List<Object> titles = new ArrayList<>();
-            List<List> listOfTitles = new ArrayList<>();
-            int i = 0;
-            //System.out.println("ID : " + resultSet.getInt("film_id"))
-           while (resultSet.next()) {
-               titles.clear();
-
-               for(int j= 1; j<=4; j++)
-               {
-                   titles.add(resultSet.getObject(j).);
-               }
-
-               listOfTitles.add(titles);
-               //System.out.println(listOfTitles);
-            /*   System.out.println("Title : " + resultSet.getString("title"));
-                System.out.println("Description : " + resultSet.getString("description"));
-                System.out.println("Year : " + resultSet.getInt("release_year"));
-                System.out.println("#################");*/
-                i++;
-            }
-
-            System.out.println(listOfTitles.get(0));
-
-            //System.out.println(stack.pop());
-        } catch (SQLException ex) {
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
+    public static void main(String[] args) {
+        List<convertToArray> userList =  convertToArray.getList();
+        Connection conn = null;
+        
+        for(convertToArray obj : userList)
+        {
+            System.out.println(obj);
         }
     }
 }
